7050d00a08d709d45a1de0da25714e6dcec7c3e0
respectus <muazs786@gmail.com>
2017-08-09 13:39:20 -0700

    now its really working

--- server/exportcsv.js
+++ server/exportcsv.js
@@ -4,76 +4,33 @@ var fs = require('fs');
 
 exports.go = function(data, callback) { // exporting the parsed data into a csv
   console.log(JSON.stringify(data, null, 2));
-  var fields = [];
-
+  var fields = ['Company Name', 'First Name', 'Last Name', 'Title', 'Email', 'Website', 'Verified', 'Reason', ' ', 'LinkedIn', 'Twitter', 'Facebook', 'Address', 'Company Size', 'Status'];
+  var newData = [];
   var next;
-  var result;
   for (var i = 0; i < data.length; i++) {
     next = data[i];
     for (var j = 0; j < next.result.length; j++) {
-            fields.push({
-                label: 'Company Name'
-                , value: next.companyDetails.name
-            }
-            , {
-                label: 'First Name'
-                , value: next.result[j].firstname
-            }
-            , {
-                label: 'Last Name'
-                , value: next.result[j].lastname
-            }
-            , {
-                label: 'Title'
-                , value: next.result[j].title
-            }
-            , {
-                label: 'Email'
-                , value: next.result[j].email
-            }
-            , {
-                label: 'Website'
-                , value: next.companyDetails.url
-            }
-            , {
-                label: 'Verified'
-                , value: next.result[j].validemail
-            }
-            , {
-                label: 'Reason'
-                , value: null
-            }
-            , {
-                label: ' '
-                , value: ' '
-            }
-            , {
-                label: 'LinkedIn'
-                , value: next.result[j].linkedin
-            }
-            , {
-                label: 'Twitter'
-                , value: next.result[j].twitter
-            }, {
-                label: 'Facebook'
-                , value: next.result[j].facebook
-            }
-            , {
-                label: 'Address'
-                , value: next.companyDetails.address
-            }
-            , {
-                label: 'Company Size'
-                , value: next.companyDetails.size
-            }
-            , {
-                label: 'Status'
-                , value: 'searched'
+            newData.push({
+                'Company Name': next.companyDetails.name
+                , 'First Name': next.result[j].firstname
+                , 'Last Name': next.result[j].lastname
+                , 'Title': next.result[j].title
+                , 'Email': next.result[j].email
+                , 'Website': next.companyDetails.url
+                , 'Verified': next.result[j].validemail
+                , 'Reason': 'Fill In'
+                , ' ': ' '
+                , 'LinkedIn': next.result[j].linkedin
+                , 'Twitter': next.result[j].twitter
+                , 'Facebook': next.result[j].facebook
+                , 'Address': next.companyDetails.address
+                , 'Company Size': next.companyDetails.size
+                , 'Status': 'searched'
             });
     }
   }
 
-  var csv = json2csv({ data: data, fields: fields}); // format of the csv
+  var csv = json2csv({ data: newData, fields: fields}); // format of the csv
 
   fs.writeFile('result.csv', csv, function(err) {
     if (err) {
